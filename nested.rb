f375330968cccf4febd03fb9a6f274d28d3b25e8
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-16 04:42:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      #alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
      
-     programmer_hash[:alan_kay][:known_for]
+     return programmer_hash[:alan_kay][:known_for]
      
 end
 
